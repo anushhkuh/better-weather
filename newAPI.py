b68a89629f6c0192e9c6355c570989265e69ddf8
ThatScienceGirl <121858743+anushhkuh@users.noreply.github.com>
2025-08-23 20:46:31 -0700

    Delete newAPI.py

--- newAPI.py
+++
@@ -1,9 +0,0 @@
-import requests
-from config import NEWSAPIKEYS
-
-def get_news():
-    url="https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWSAPIKEYS}"
-    response= requests.get(url)
-    if response.status_code == 200:
-        return response.json().get("articles", []) #key value pair query
-    return []
